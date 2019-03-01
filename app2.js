e0a36f6ea19024c0a252778d564ab620575290ed
colinskeep <colinjinskeep@gmail.com>
2019-02-28 23:03:47 -0700

    commit

---
+++ app2.js
@@ -0,0 +1,59 @@
+const puppeteer = require('puppeteer');
+require('dotenv').config();
+
+try {
+  (async () => {
+    const browser = await puppeteer.launch({
+      headless: false, // launch headful mode
+    });
+    const page = await browser.newPage();
+    await page.goto('https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&flowName=GlifWebSignIn&flowEntry=AddSession');
+    await page.type('.whsOnd.zHQkBf', 'bankers.schedule11@gmail.com');
+    await page.click('div[id="identifierNext"]');
+    await page.waitFor(2000);
+    await page.type('.whsOnd.zHQkBf', process.env.USER_PW);
+    await page.click('div[id="passwordNext"]');
+    await page.waitFor(2000);
+    if (page.url().indexOf('https://accounts.google.com/signin/v2/challenge/') > -1){
+      console.log('verification needed');
+      await page.click('div[class="vdE7Oc"]');
+      await page.waitFor(2000);
+      await page.type('.whsOnd.zHQkBf', process.env.USER_RECOVERY_EMAIL);
+      await page.click('div[class="ZFr60d CeoRYc"]');
+      await page.waitFor(2000);
+      await page.click('div[class="ZFr60d CeoRYc"]');
+    }
+    console.log('logged in: ', 'bankers.schedule11@gmail.com')
+    await page.goto('https://www.youtube.com/channel_switcher');
+    console.log('checking brand accounts.....')
+    await page.waitFor(2000);
+    const hrefs = await page.$$eval('a', as => as.map(a => a.href));
+    let arr = [];
+    for (let i = 0; i < hrefs.length; i++) {
+      if (hrefs[i].indexOf('pageid') > - 1) {
+        arr.push(hrefs[i]);
+        }
+        if (i == hrefs.length - 1) {
+          console.log('found ',arr.length, 'brand accounts for', 'bankers.schedule11@gmail.com');
+          for (let x = 0; x < arr.length; x++) {
+            await page.goto(arr[x]);
+            await page.waitFor(2000);
+            if (page.url().indexOf('https://accounts.google.com/signin/v2/') > -1) {
+              await page.waitFor(2000);
+              await page.type('.whsOnd.zHQkBf', process.env.USER_PW);
+              await page.click('div[id="passwordNext"]');
+            }
+            await page.waitFor(2000);
+            await page.goto(`https://www.youtube.com/watch?v=perRLsgWvng`)
+            await page.waitFor(2000);
+            await page.evaluate(() => {
+              if (document.querySelector("a.yt-simple-endpoint.style-scope.ytd-toggle-button-renderer").innerHTML.indexOf('aria-pressed="true"') == -1 ){
+                document.querySelector("a.yt-simple-endpoint.style-scope.ytd-toggle-button-renderer").click()
+              }
+              else(console.log("not found"))
+            });
+          }
+      }
+    }
+  })()
+} catch (err) { console.log(err) }
